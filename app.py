e82fadba45274b043d82237ec50a718de1bfa8ff
JackWang <99715727+JackWang0318@users.noreply.github.com>
2023-06-07 14:34:08 +0800

    update

---
+++ app.py
@@ -0,0 +1,219 @@
+#frontend lib
+import cv2
+import streamlit as st
+import numpy as np
+from PIL import Image
+import requests
+import mediapipe as mp
+import av
+
+#app lib
+from matplotlib import pyplot as plt
+from scipy import stats
+from sklearn.model_selection import train_test_split
+#from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
+from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
+
+#import demo as dm
+# Actions that we try to detect
+actions = np.array(['hello', 'thanks', 'iloveyou'])
+
+# Thirty videos worth of data
+no_sequences = 30
+
+# Videos are going to be 30 frames in length
+sequence_length = 30
+
+# Folder start
+start_folder = 30
+
+colors = [(245,117,16), (117,245,16), (16,117,245)]
+
+#------------------Keypoints using MP Holistic------------------
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS) # Draw face connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
+
+def draw_styled_landmarks(image, results):
+    # Draw face connections
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS, 
+                             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+                             mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+                             ) 
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                             ) 
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                             ) 
+    # Draw right hand connections  
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                             ) 
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, face, lh, rh])
+
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
+    return output_frame
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+#you can train this model,or use action_h5/action_pre.h5
+#model.fit(X_train, y_train, epochs=2000, callbacks=[tb_callback])
+model.summary()
+model.load_weights('action.h5')
+
+
+def web_cam():
+    # 创建一个空的用于显示视频的图像容器    
+    cap = cv2.VideoCapture(0)
+    video_container = st.empty()
+
+
+    sequence = []
+    sentence = []
+    predictions = []
+    threshold = 0.5
+    # Set mediapipe model 
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
+
+            # Read feed
+            ret, frame = cap.read()
+
+            # Make detections
+            image, results = mediapipe_detection(frame, holistic)
+            print(results)
+            
+            # Draw landmarks
+            draw_styled_landmarks(image, results)
+            
+            # 2. Prediction logic
+            keypoints = extract_keypoints(results)
+            sequence.append(keypoints)
+            sequence = sequence[-30:]
+            
+            if len(sequence) == 30:
+                res = model.predict(np.expand_dims(sequence, axis=0))[0]
+                print(actions[np.argmax(res)])
+                predictions.append(np.argmax(res))
+                
+                
+            #3. Viz logic
+                if np.unique(predictions[-10:])[0]==np.argmax(res): 
+                    if res[np.argmax(res)] > threshold: 
+                        
+                        if len(sentence) > 0: 
+                            if actions[np.argmax(res)] != sentence[-1]:
+                                sentence.append(actions[np.argmax(res)])
+                        else:
+                            sentence.append(actions[np.argmax(res)])
+
+                if len(sentence) > 5: 
+                    sentence = sentence[-5:]
+
+                # Viz probabilities
+                image = prob_viz(res, actions, image, colors)
+                
+            cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+            cv2.putText(image, ' '.join(sentence), (3,30), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            video_container.image(image, channels="BGR")
+            # Show to screen
+            #cv2.imshow('OpenCV Feed', image)
+
+            # Break gracefully
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+
+
+def main():
+    # 设置应用程序的主题
+    # st.set_page_config(page_title="ML_Web_App", page_icon=":smiley:", 
+    #                    layout="wide", initial_sidebar_state="expanded", theme="dark")
+    
+    st.title("Action Detection Web App")
+    st.subheader("This app allows you to play with hand gesture using machine learning!")
+    st.text("ML01 Machine Learning, UTSEUS, Shanghai University")
+
+    #sidebar experiment
+    # Using object notation
+    add_selectbox = st.sidebar.selectbox(
+        "Would you like to spend time learning ML?",
+        ("Absolutely yes", "Possibly", "Never")
+    )
+    # Using "with" notation
+    with st.sidebar:
+        add_radio = st.radio(
+            "How do you like our project?",
+            ("Not much", "Just so-so", "Great", "Perfect")
+        )
+        
+    #Using tabs
+    tab1, tab2, tab3, tab4 = st.tabs(["hello", "thanks", "i love you", "i hate you"])
+    with tab1:
+        st.header("hello")
+        st.image("example/hello.png", width=500)
+
+    with tab2:
+        st.header("thanks")
+        st.image("example/thanks.png", width=500)
+
+    with tab3:
+        st.header("i love you")
+        st.image("example/iloveyou.png", width=500)
+
+    with tab4:
+        st.header("i hate you")
+        st.image("example/ihateyou.png", width=500) 
+
+    button_clicked = st.checkbox("Open My Web Camera", key="primary_button")
+    # 检查按钮是否被点击 
+    if button_clicked:
+         web_cam()
+
+if __name__ == '__main__':
+    main()
